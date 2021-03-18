e21cc5dcb33b5d28492b249ce4641252d920b99a
montypyle <77437020+montypyle@users.noreply.github.com>
2021-03-18 09:08:00 +0000

    setting up enemy scripts

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody2D enemyRB;
+    private bool facingRight;
+    private Transform enemyPivot;
+    private Vector3 startingDirection;
+    public float enemySpeed;
     void Start()
     {
-        
+        enemyRB = this.GetComponent<Rigidbody2D>();
+        enemyPivot = this.GetComponent<Transform>();
+        //this checks whether the enemy starts off facing right in the scene or not
+        enemyPivot.transform.localScale = startingDirection;
+        if (startingDirection.x == -1)
+        {
+            facingRight = true;
+        }
+        else
+        {
+            facingRight = false;
+        }
+        Patrol();
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    void Patrol()
+    {
+        enemyRB.velocity = new Vector2(enemySpeed, enemyRB.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Flip();
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = enemyPivot.transform.localScale;
+        theScale.x *= -1;
+        enemyPivot.transform.localScale = theScale;
     }
 }
