9fd42318eb68dae4c4aa34fe39a2b0d0496235e3
montypyle <77437020+montypyle@users.noreply.github.com>
2021-04-27 15:59:38 +0100

    added animations to main menu

---
+++ Assets/Scripts/PortraitHandler.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitHandler : MonoBehaviour
+{
+    private Animator portraitAnimator;
+    void Start()
+    {
+        portraitAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AnimatePortrait()
+    {
+        portraitAnimator.SetBool("Activated", true);
+    }
+}
